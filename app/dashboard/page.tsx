330fe5b3b572cbf0708a992fab797fe5142763bc
Satvik Katoch <katochsatvik@gmail.com>
2025-07-22 23:53:50 +0530

    feat: pass fetched data and last updated timestamp to SectionCards and ChartAreaInteractive components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -98,7 +98,7 @@ export default function Page() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
+              <SectionCards data={data} />
 
               <div className="px-4 lg:px-6">
                 <ChartAreaInteractive data={data} lastUpdated={lastUpdated} />
